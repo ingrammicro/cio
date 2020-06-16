5c91fe289e82348afc9accd68c0f88169c5a9710
Pablo Cantera <pablo.cantera@ingrammicro.com>
2020-06-16 16:00:26 +0200

    Added API test cases (issue #55)
    Closes #55

---
+++ api/settings/policy_definitions_api_test.go
@@ -0,0 +1,72 @@
+package settings
+
+import (
+	"github.com/ingrammicro/cio/testdata"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewPolicyDefinitionServiceNil(t *testing.T) {
+	assert := assert.New(t)
+	rs, err := NewPolicyDefinitionService(nil)
+	assert.Nil(rs, "Uninitialized service should return nil")
+	assert.NotNil(err, "Uninitialized service should return error")
+}
+
+func TestListDefinitions(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	ListDefinitionsMocked(t, policyDefinitionsIn)
+	ListDefinitionsFailErrMocked(t, policyDefinitionsIn)
+	ListDefinitionsFailStatusMocked(t, policyDefinitionsIn)
+	ListDefinitionsFailJSONMocked(t, policyDefinitionsIn)
+}
+
+func TestGetDefinition(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	for _, policyDefinitionIn := range policyDefinitionsIn {
+		GetDefinitionMocked(t, policyDefinitionIn)
+		GetDefinitionFailErrMocked(t, policyDefinitionIn)
+		GetDefinitionFailStatusMocked(t, policyDefinitionIn)
+		GetDefinitionFailJSONMocked(t, policyDefinitionIn)
+	}
+}
+
+func TestCreateDefinition(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	for _, policyDefinitionIn := range policyDefinitionsIn {
+		CreateDefinitionMocked(t, policyDefinitionIn)
+		CreateDefinitionFailErrMocked(t, policyDefinitionIn)
+		CreateDefinitionFailStatusMocked(t, policyDefinitionIn)
+		CreateDefinitionFailJSONMocked(t, policyDefinitionIn)
+	}
+}
+
+func TestUpdateDefinition(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	for _, policyDefinitionIn := range policyDefinitionsIn {
+		UpdateDefinitionMocked(t, policyDefinitionIn)
+		UpdateDefinitionFailErrMocked(t, policyDefinitionIn)
+		UpdateDefinitionFailStatusMocked(t, policyDefinitionIn)
+		UpdateDefinitionFailJSONMocked(t, policyDefinitionIn)
+	}
+}
+
+func TestDeleteDefinition(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	for _, policyDefinitionIn := range policyDefinitionsIn {
+		DeleteDefinitionMocked(t, policyDefinitionIn)
+		DeleteDefinitionFailErrMocked(t, policyDefinitionIn)
+		DeleteDefinitionFailStatusMocked(t, policyDefinitionIn)
+	}
+}
+
+func TestListDefinitionAssignments(t *testing.T) {
+	policyDefinitionsIn := testdata.GetPolicyDefinitionData()
+	policyAssignmentsIn := testdata.GetPolicyAssignmentData()
+	for _, policyDefinitionIn := range policyDefinitionsIn {
+		ListDefinitionAssignmentsMocked(t, policyDefinitionIn.ID, policyAssignmentsIn)
+		ListDefinitionAssignmentsFailErrMocked(t, policyDefinitionIn.ID, policyAssignmentsIn)
+		ListDefinitionAssignmentsFailStatusMocked(t, policyDefinitionIn.ID, policyAssignmentsIn)
+		ListDefinitionAssignmentsFailJSONMocked(t, policyDefinitionIn.ID, policyAssignmentsIn)
+	}
+}
--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/urfave/cli v1.22.4
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
