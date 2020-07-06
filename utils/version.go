a1f22dfb99f431f1ab0280a19bb454cf1ee4278d
Pablo Cantera <pablo.cantera@ingrammicro.com>
2020-07-06 11:33:12 +0200

    Prepare release of v0.12.0-beta :construction:

--- utils/version.go
+++ utils/version.go
@@ -1,7 +1,7 @@
 package utils
 
 // VERSION sets the current CLI version
-const VERSION = "0.11.0"
+const VERSION = "0.12.0-beta"
 
 // VERSION_API_USER_MODE sets the current IMCO API version for user mode
 const VERSION_API_USER_MODE = "v2"
