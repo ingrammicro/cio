0c952cb44489be87ddbece0349450a9ecd6242af
Pablo Cantera <pablo.cantera@ingrammicro.com>
2021-07-02 12:46:51 +0200

    Prepare release of v0.13.0 :tada:

--- utils/version.go
+++ utils/version.go
@@ -3,7 +3,7 @@
 package utils
 
 // VERSION sets the current CLI version
-const VERSION = "0.13.0-rc"
+const VERSION = "0.13.0"
 
 // VERSION_API_USER_MODE sets the current IMCO API version for user mode
 const VERSION_API_USER_MODE = "v3"
