ea36f5e86d64cfaa0595364a3efa3ce4be927a70
Pablo Baños López <pablo.banos@ingrammicro.com>
2022-10-11 15:26:35 +0200

    Prepare release of v0.15.1-rc :construction:

--- utils/version.go
+++ utils/version.go
@@ -3,7 +3,7 @@
 package utils
 
 // VERSION sets the current CLI version
-const VERSION = "0.15.1-beta"
+const VERSION = "0.15.1-rc"
 
 // VERSION_API_USER_MODE sets the current IMCO API version for user mode
 const VERSION_API_USER_MODE = "v3"
